2de380f21844136290dd22da6bff213f36c5c4dd
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-11 23:30:36 +0100

    .inject or .sum better

--- betterThanAverage.rb
+++ betterThanAverage.rb
@@ -4,9 +4,13 @@
 # Return True if you're better, else False!
 
 def better_than_average(arr, points)
-  sum = 0
-  arr.each { |a| sum += a }
-  points > sum / arr.count
+  points > arr.inject(:+) / arr.length
+  ## or arr.sum
 end
 
 better_than_average([100, 40, 34, 57, 29, 72, 57, 88], 75)
+
+
+
+
+
